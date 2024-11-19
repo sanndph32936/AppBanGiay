dc81656f7337825bc16ad8d1af3c504bc1a0a960
Nguyen Ducc San PH32936 <sanndph32936fpt.edu.vn>
2024-11-19 22:55:04 +0700

    models

---
+++ app/src/main/java/com/example/myapplication/models/GioHang.java
@@ -0,0 +1,81 @@
+package com.example.myapplication.models;
+
+
+
+import java.io.Serializable;
+
+public class GioHang implements Serializable {
+
+    String anhSP,tenSP,sizeSP,giaSP;
+    String soluongSP;
+    String idSP,idNguoiDung;
+
+    public GioHang() {
+    }
+
+    public GioHang(String anhSP, String tenSP, String sizeSP, String giaSP, String soluongSP, String idSP, String idNguoiDung) {
+        this.anhSP = anhSP;
+        this.tenSP = tenSP;
+        this.sizeSP = sizeSP;
+        this.giaSP = giaSP;
+        this.soluongSP = soluongSP;
+        this.idSP = idSP;
+        this.idNguoiDung = idNguoiDung;
+    }
+
+    public String getIdNguoiDung() {
+        return idNguoiDung;
+    }
+
+    public void setIdNguoiDung(String idNguoiDung) {
+        this.idNguoiDung = idNguoiDung;
+    }
+
+    public String getAnhSP() {
+        return anhSP;
+    }
+
+    public void setAnhSP(String anhSP) {
+        this.anhSP = anhSP;
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public String getSizeSP() {
+        return sizeSP;
+    }
+
+    public void setSizeSP(String sizeSP) {
+        this.sizeSP = sizeSP;
+    }
+
+    public String getGiaSP() {
+        return giaSP;
+    }
+
+    public void setGiaSP(String giaSP) {
+        this.giaSP = giaSP;
+    }
+
+    public String getSoluongSP() {
+        return soluongSP;
+    }
+
+    public void setSoluongSP(String soluongSP) {
+        this.soluongSP = soluongSP;
+    }
+
+    public String getIdSP() {
+        return idSP;
+    }
+
+    public void setIdSP(String idSP) {
+        this.idSP = idSP;
+    }
+}
